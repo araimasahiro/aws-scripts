a4300874ff41d9c659e6a9fda2586bc5bd0caebc
araimasahiro <masahiro@araike.net>
2024-11-22 16:28:08 +0900

    [modify] support RHEL and SUSE

--- update-ami.sh
+++ update-ami.sh
@@ -29,8 +29,8 @@ for ARCH in x86_64 arm64; do
     --output json \
     | jq -r '.[]|[.ImageId, .Architecture, .Description] | @tsv')
     echo "# $_ret"
-    echo "export AMI_AL2023_$ARCH=$(echo "$_ret" | awk -F'\t' '{print $1 " #" $3}')" >> "$AMI_ENV_FILE"
-    #Ubuntu 22.04, 20.04
+    echo "export AMI_AmazonLinux2023_$ARCH=$(echo "$_ret" | awk -F'\t' '{print $1 " #" $3}')" >> "$AMI_ENV_FILE"
+    #Ubuntu 24.04 noble, 22.04 jammy, 20.04 focal
     for VER in noble jammy focal; do
         _ret=$(aws ec2 describe-images \
         --region "$REGION" \
@@ -42,5 +42,28 @@ for ARCH in x86_64 arm64; do
         echo "# $_ret"
         echo "export AMI_Ubuntu_${VER}_${ARCH}=$(echo "$_ret" | awk -F'\t' '{print $1 " #" $3}')" >> "$AMI_ENV_FILE"
     done
+    #SUSE Linux Enterprise Server 15
+    for VER in 15 12; do
+        if [ "$VER" = "12" ] && [ "$ARCH" = "arm64" ]; then break; fi
+        _ret=$(aws ec2 describe-images \
+        --region "$REGION" \
+        --owners amazon \
+        --query 'reverse(sort_by(Images, &CreationDate))[:1]' \
+        --filters "Name=name,Values=suse-sles-$VER-sp*-v????????-hvm-ssd-*" "Name=architecture,Values=$ARCH" \
+        --output json \
+        | jq -r '.[]|[.ImageId, .Architecture, .Description] | @tsv')
+        echo "# $_ret"
+        echo "export AMI_SUSE${VER}_${ARCH}=$(echo "$_ret" | awk -F'\t' '{print $1 " #" $3}')" >> "$AMI_ENV_FILE"
+    done
+    #Redhat Enterprise Linux 9
+    _ret=$(aws ec2 describe-images \
+    --region "$REGION" \
+    --owners amazon \
+    --query 'reverse(sort_by(Images, &CreationDate))[:1]' \
+    --filters "Name=name,Values=RHEL-9.*_HVM-*-$ARCH-*" "Name=architecture,Values=$ARCH" \
+    --output json \
+    | jq -r '.[]|[.ImageId, .Architecture, .Description] | @tsv')
+    echo "# $_ret"
+    echo "export AMI_RHEL9_$ARCH=$(echo "$_ret" | awk -F'\t' '{print $1 " #" $3}')" >> "$AMI_ENV_FILE"    
 done
 echo "Export environment value: $AMI_ENV_FILE"
